4192d868e4dfc9983a89f7909c45d09482e10949
megakirill <78233797+megakirill@users.noreply.github.com>
2024-12-08 15:27:39 +0300

    add cd, ls

---
+++ homework_1/shell_emulator.py
@@ -0,0 +1,84 @@
+import os
+import tarfile
+import csv
+
+class Shell_emulator:
+    def __init__(self, config):
+        self.load_config(config)
+        self.load_virtual_fs_tar()
+        self.cwd = '/'
+        self.history = []
+        self.output = []
+        self.log_actions = []
+
+
+    def load_virtual_fs_tar(self):
+        # Открываем tar-архив для чтения
+        self.tar_file = tarfile.open(self.zip_path, 'r')
+
+        # Получаем список всех файлов и директорий из архива
+        self.files = list(set(member.name for member in self.tar_file.getmembers()))
+        print(self.files)
+
+    def load_config(self, config_path):
+        with open(config_path, 'r') as f:
+            reader = csv.reader(f)
+            config = {row[0]: row[1] for row in reader}  # Преобразуем CSV в словарь
+
+            # Присваиваем значения соответствующим атрибутам
+            self.computer_name = config.get('computer.name', '')
+            self.zip_path = config.get('filesystem.path', '')
+            self.startup_script = config.get('startup.script', '')
+            self.log_path = config.get('logging.path', '')
+
+    def cd(self, path):
+        current = self.cwd
+        if path == '..':
+            self.cwd = os.path.dirname(self.cwd.rstrip('/'))
+            if not self.cwd:
+                self.cwd = '/'
+        else:
+            new_path = os.path.join(self.cwd.rstrip('/'), path).replace('\\', '/')
+            if any(file.startswith('./' + new_path) or file == new_path for file in self.files):
+                self.cwd = new_path
+                print(f"Changed directory to {self.cwd}")
+            else:
+                print(new_path)
+                print(f"Directory '{path}' not found")
+        print(self.cwd)
+
+    def ls(self):
+        if self.cwd == '/':
+            cwd_with_slash = './'
+        else:
+            cwd_with_slash = './' + self.cwd + '/'
+        items = set()
+        for file in self.files:
+            if file.startswith(cwd_with_slash) and cwd_with_slash != file:
+                relative_path = file[len(cwd_with_slash):].split('/')[0]
+                items.add(relative_path)
+        print(items)
+
+    def rev(self, file):
+        full_path = os.path.join(self.cwd.rstrip('/'), file).replace('\\', '/').lstrip('/')
+        if './' + full_path in self.files:
+            try:
+                member = self.tar_file.getmember('./' + full_path)
+                print(f"Found member: {member.name}, is file: {member.isfile()}, is dir: {member.isdir()}")
+
+                # Убедимся, что это файл, а не директория
+                if member.isfile():
+                    with self.tar_file.extract(member) as f:
+                        print(f.read())
+            except KeyError:
+                print('Не найден в архиве')
+
+
+
+a = Shell_emulator('config.csv')
+a.ls()
+a.cd('some_directory')
+a.rev('some_file.txt')
+x
+
+
